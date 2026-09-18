ff232c2b2a13bb2dbf49d36acde9f38e1a022f75
Angry548 <angelmate827@gmail.com>
2026-09-18 14:46:09 -0600

    Cambios para la Practica 20260910

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const estudianteRoutes = require('./routes/estudiante.routes');
 const cursoRoutes = require('./routes/curso.routes');
+const usuarioRoutes = require('./routes/usuario.routes');
 
 const app = express();
 
-app.use(express.json());
+// Configuración de CORS
+const corsOptions = {
+  origin: process.env.CLIENT_URL || '*', // O especifica tu frontend (ej: 'http://localhost:5173')
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
-// Inyección de rutas semánticas
+app.use(cors(corsOptions)); // Aplicar CORS globalmente
+app.use(express.json());
+app.use(cookieParser());
+
+// Registro de Rutas
+app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/estudiantes', estudianteRoutes);
 app.use('/api/cursos', cursoRoutes);
 
